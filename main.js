570149083f42985520e1f8e102ffd06f7b3c856e
SomeArtem <serebryakov.artemiy@mail.ru>
2023-03-23 00:41:33 +0300

    'js_rendered_elements'

--- main.js
+++ main.js
@@ -3,23 +3,69 @@ import './public/style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    хуй жопа
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="red_class">Привет, Мир!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const services_container = document.querySelector('#services_target')
+const services=[
+  {
+    service_logo_src:'',
+    service_date:'12 oct 2019',
+    service_status:'Winner',
+    service_award:'MSMI Media UX Award 2018-19',
+    service_location:'Torrento, Canada'
+  },
+  {
+    service_logo_src:'',
+    service_date:'28 dec 2019',
+    service_status:'Gold Winner',
+    service_award:'Apple Design Award 2018-19',
+    service_location:'United Statues'
+  },
+  {
+    service_logo_src:'',
+    service_date:'28 nov 2019',
+    service_status:'Asia Pacific',
+    service_award:'Yellow Dot Design Award 2019-20',
+    service_location:'United Nation'
+  },
+  {
+    service_logo_src:'',
+    service_date:'28 nov 2019',
+    service_status:'Runner up',
+    service_award:'Indiana Best Design 2019-20',
+    service_location:'North Amurika'
+  },
+  {
+    service_logo_src:'',
+    service_date:'30 dec 2019',
+    service_status:'Silver Winner',
+    service_award:'UNO UX India Award 2020-21',
+    service_location:'Hyderbad, India'    
+  },
+  {
+    service_logo_src:'',
+    service_date:'13 aug 2019',
+    service_status:'Winner',
+    service_award:'Best Music Album 2018-19',
+    service_location:'Mumbai, India'
+  }
+]
 
-setupCounter(document.querySelector('#counter'))
+for (let i = 0; i < services.length; i++) {
+  let element = services[i];
+  let el=document.createElement("div");
+  el.classList+='service';
+  el.innerHTML=`  
+  <div class="service_head" >
+    <div class="service_logo"></div>
+    <div class="service_date">${element.service_date}</div>
+  </div>
+  <div class="service_body">
+    <div class="service_status">${element.service_status}</div>
+    <div class="service_award">${element.service_award}</div>
+  </div>
+  <div class="service_footer">
+    <div class="service_location">${element.service_award}</div>
+  </div>`  
+  services_container.appendChild(el)
+}
+
+// setupCounter(document.querySelector('#counter'))
