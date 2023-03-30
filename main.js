f0fb118972f799098cd303e329eb0dc0dcf4a32e
SomeArtem <serebryakov.artemiy@mail.ru>
2023-03-30 21:09:39 +0300

    mdoified

--- main.js
+++ main.js
@@ -1,25 +1,5 @@
 import './public/style.css'
 //import './reset.css'
 import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "uno.css"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    хуй жопа
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="red_class">Привет, Мир!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,48 @@
 import host from 'vite-plugin-host'
-
+import UnoCSS from 'unocss/vite'
+import presetUno from "unocss/preset-uno"
+import presetIcons from '@unocss/preset-icons'
+import presetWebFonts from '@unocss/preset-web-fonts'
 
 
 export default {
   plugins: [    
-    host(),    
+    host(),
+    UnoCSS({
+      presets: [
+        presetUno(), // disable default preset
+        presetIcons({ /* options */ }),
+        presetWebFonts({
+          provider: 'google', // default provider
+          fonts: {
+            // these will extend the default theme
+            sans: 'Roboto',
+            mono: ['Fira Code', 'Fira Mono:400,700'],
+            // custom ones
+            lobster: 'Lobster',
+            delicious_handrawn: [{
+              name:'Delicious Handrawn', 
+              cursive: true
+            }],
+            poppins:'Poppins',
+            lato: [
+              {
+                name: 'Lato',
+                weights: ['400', '700'],
+                italic: true,
+              },
+              {
+                name: 'sans-serif',
+                provider: 'none',
+              },
+            ],
+          },
+        }),
+      ], 
+      // rules: [
+      //   // your custom rules
+      // ],
+    }),   
   ],
   build:{
     
