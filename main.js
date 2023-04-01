a42bdd111b294709e7e0c2a976a080cd0eb86935
SomeArtem <Serebryakov.artemiy@mail.ru>
2023-04-01 11:30:24 +0300

    js_lesson

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
-import './public/style.css'
-//import './reset.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+//строки
+// let myf2name=new String('Artemd');
+// let year = 1997;
+// let isHappy=true;
+// let friendname='alexandr';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    хуй жопа
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1 class="red_class">Привет, Мир!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// console.log(myf2name);
+// console.log('John '+'Maxwell '+(year+' '+year))
+// console.log(myf2name[3])
 
-setupCounter(document.querySelector('#counter'))
+
+//массивы
+// let fibonacci=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
+// let arr=new Array(10);
+// console.log(fibonacci[1], `\n длина массива arr - ${arr.length}`)
+
+//obj
+// class User{
+//   constructor(name, age){
+//     this.name=name
+//     this.age=age
+//   }
+// }
+
+// let user={
+//   name:'Vladimir',
+//   age:37
+// }
+// let user2=new User('Volodymir', 50)
+// let user3=new User('Shoygoo', 73)
+// console.log(user2['name'], user2.age, '\n', user3.name, user3.age, user3?.weight)
+
+// console.log('John Bolton'.slice(0, 3))
+// console.log('John Bolton'.substring(0, 3))
+
+
+//браузерный api
+// let app=document.body.children[0]
+// app.textContent='Volodymir'
+// console.log()
