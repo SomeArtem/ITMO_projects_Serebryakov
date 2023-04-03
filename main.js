7b4d8d889f9baefa2bbc5adfa6c41efe8461e0e8
SomeArtem <serebryakov.artemiy@mail.ru>
2023-04-04 00:09:06 +0300

    'ready'

---
+++ main.js
@@ -0,0 +1,30 @@
+const btns=document.getElementsByClassName('btn_anchor')
+const display=document.getElementById('display')
+const btnCalc=document.querySelector('.btn_calc')
+const btnClear=document.querySelector('.btn_clear')
+let operCatcher=document.getElementById('oper')
+
+
+
+for (let i = 0; i < btns.length; i++) {
+    const element = btns[i];
+    element.addEventListener('click', (e)=>{
+        display.innerHTML+= e.target.innerText
+        operCatcher.removeAttribute('disabled')
+    })    
+}
+
+
+operCatcher.addEventListener("change",e=>{
+    display.innerText+= e.target.value      
+});
+
+btnCalc.addEventListener('click',e=>{
+    display.innerText=eval(display.innerText)
+    operCatcher.value='shithead'
+})
+
+btnClear.addEventListener('click',e=>{
+    display.innerText=''
+    operCatcher.setAttribute('disabled','disabled')
+})
