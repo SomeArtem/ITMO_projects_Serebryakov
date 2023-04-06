ffe1b8fee51622fa7ffb333281b6474328a0bc7f
SomeArtem <serebryakov.artemiy@mail.ru>
2023-04-06 19:33:26 +0300

    'asfsafasf'

--- main.js
+++ main.js
@@ -3,3 +3,18 @@ import './public/style.css'
 import javascriptLogo from './javascript.svg'
 import "uno.css"
 
+const DOM=(id)=>document.getElementById(id)
+
+const DomTemplateTask=DOM('templateTask')
+const DOMTaskColumn=DomTemplateTask.parentNode;
+
+
+[1,2,3,4].forEach( (item) => {
+    const DOMTaskClone=DomTemplateTask.cloneNode(true);
+    DOMTaskColumn.prepend(DOMTaskClone);
+})
+
+
+DOMTaskColumn.removeChild(DomTemplateTask)
+
+console.log('Main', DomTemplateTask)
