10aa96cacf5cd6396e4ef0cd217cbd6bc00699a6
SomeArtem <serebryakov.artemiy@mail.ru>
2023-04-08 10:13:11 +0300

    'popup_added'

--- main.js
+++ main.js
@@ -3,3 +3,14 @@ import './public/style.css'
 import javascriptLogo from './javascript.svg'
 import "uno.css"
 
+
+let popup=document.querySelector('#popupCreateTask')
+document.querySelector('#createButton').addEventListener('click', (e)=>{
+    popup.classList.toggle('hidden')    
+})
+
+
+
+document.querySelector('#closeBtn').addEventListener('click', (e)=>{
+    popup.classList.toggle('hidden')    
+})
