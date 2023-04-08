e69c30ca4931c916ce4b2ba18339b7d59dbefcd8
SomeArtem <serebryakov.artemiy@mail.ru>
2023-04-08 12:07:58 +0300

    'keys_added'

--- main.js
+++ main.js
@@ -2,15 +2,22 @@ import './public/style.css'
 //import './reset.css'
 import javascriptLogo from './javascript.svg'
 import "uno.css"
+import Dom from "./src/constants/keys"
 
+//функции поиска элементов
+const getDOM=(selector)=>document.querySelector(selector);
+const FIND = (container, selector)=>container.querySelector(selector)
 
-let popup=document.querySelector('#popupCreateTask')
-document.querySelector('#createButton').addEventListener('click', (e)=>{
-    popup.classList.toggle('hidden')    
-})
-
-
-
-document.querySelector('#closeBtn').addEventListener('click', (e)=>{
-    popup.classList.toggle('hidden')    
-})
+//открывает popup
+getDOM(Dom.Button.CREATE_BUTTON).onclick=()=>{    
+    const DomPopCreateTask=getDOM(Dom.Popup.ID);   
+    const CloseButton=FIND(DomPopCreateTask, Dom.Popup.CLOSE_BUTTON);    
+    
+    DomPopCreateTask.classList.toggle('hidden');
+    
+    //закрывает popup
+    CloseButton.onclick = ()=>{
+        DomPopCreateTask.classList.toggle('hidden');
+        CloseButton.onclick=null;
+    }
+}
---
+++ src/constants/keys.js
@@ -0,0 +1,12 @@
+const Dom={
+        Button:{
+            CREATE_BUTTON: '#createButton',
+        },
+        Popup:{
+            ID:"#popupCreateTask",
+            CLOSE_BUTTON:"#closeBtn",
+        }
+    }
+
+
+export default Dom;
