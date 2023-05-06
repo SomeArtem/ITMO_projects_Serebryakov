f02d717d7b501b1183bfe6f0cf9f23df5969379a
SomeArtem <serebryakov.artemiy@mail.ru>
2023-05-06 21:24:49 +0300

    'monsters_working_finaly'

--- main.js
+++ main.js
@@ -1,84 +1,72 @@
-//строки
-// let myf2name=new String('Artemd');
-// let year = 1997;
-// let isHappy=true;
-// let friendname='alexandr';
+const App=document.getElementsByClassName('App')[0];
 
-// console.log(myf2name);
-// console.log('John '+'Maxwell '+(year+' '+year))
-// console.log(myf2name[3])
+//настройки
+const PIXEL_SIZE=50;
+const PIXEL_COLOR_2='black';
+const PIXEL_COLOR_1='red';
+const MONSTER_SIZE=6
+const ROWPART_SIZE=MONSTER_SIZE/2;
+
+const Colors=[];
+let ID=0;
 
 
-//массивы
-// let fibonacci=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233]
-// let arr=new Array(10);
-// console.log(fibonacci[1], `\n длина массива arr - ${arr.length}`)
 
-//obj
-// class User{
-//   constructor(name, age){
-//     this.name=name
-//     this.age=age
-//   }
+const CreateBlock = (blockSize, blockColor, blockShiftX) => {
+    const block=document.createElement('div');
+    block.style.backgroundColor=blockColor;
+    block.style.width= block.style.height=`${blockSize}px`;
+    block.style.position='absolute';
+    block.style.left=`${blockShiftX}px`;
+    block.id=ID++;
+    App.appendChild(block)
+    // Arr.push(block);
+}
+
+const ColorSwitcher = () =>{
+    return Math.random()>0.5 ? PIXEL_COLOR_1 : PIXEL_COLOR_2
+}
+
+const CreateRowPart = (startPosition) => {
+    for (let i = 0; i < ROWPART_SIZE; i++) {
+        randomColor=ColorSwitcher();
+        Colors.push(randomColor);
+        CreateBlock(PIXEL_SIZE, randomColor, startPosition+PIXEL_SIZE*i);
+    }
+}
+
+CreateRowPart(0);
+CreateRowPart(ROWPART_SIZE*PIXEL_SIZE);
+
+let ArrBackPart=Colors.slice(0,Colors.length/2).reverse()
+console.log(ArrBackPart)
+Colors.splice(Colors.length/2,Colors.length/2,...ArrBackPart)
+
+for (let i = 0; i < Colors.length; i++) {
+    const element = Colors[i];
+    const blockk= document.getElementById(i);
+    blockk.style.backgroundColor=element;
+    App.appendChild(blockk);
+}
+
+// const CreateRow=()=>{
+//     CreateRowPart(0);
+//     CreateRowPart(ROWPART_SIZE*PIXEL_SIZE);
+//     Arr.push(Arr.reverse())
+//     for (let i = 0; i < Arr.length; i++) {
+//         const element = Arr[i];
+
+//         App.appendChild(element)
+//         console.log(element.style.left)   
+        
+//     }
 // }
 
-// let user={
-//   name:'Vladimir',
-//   age:37
-// }
-// let user2=new User('Volodymir', 50)
-// let user3=new User('Shoygoo', 73)
-// console.log(user2['name'], user2.age, '\n', user3.name, user3.age, user3?.weight)
-
-// console.log('John Bolton'.slice(0, 3))
-// console.log('John Bolton'.substring(0, 3))
+// CreateRow()
+// console.log(Arr);
 
 
-//браузерный api
-let app=document.body.children[0]
-app.textContent='Volodymir'
-console.log()
-
-//функции
-const DOM = document.getElementById.bind(document)
-const domInpName=DOM('inpName')
-const domInpSurname=DOM('inpSurname')
-const domConResult=DOM('result')
-let addResult=add(10,3)
-
-function add(a,b=5){
-    const result=a+b
-    console.log("a+b: "+result)
-    return result
-}
-
-console.log('addresult:', {addResult})
-console.log(domInpName.value, domInpSurname.value)
-
-domInpName.oninput=oninpchange;
-domInpSurname.oninput=oninpchangesur;
-let firstname=''
-let surname=''
-let fullname=''
 
 
-function oninpchange(e){
-    firstname=e.target.value
-    //console.log('oninpchange: ',firstname)    
-    e.stopPropagation();    
-    fullname=firstname+' '+surname
-    renderName(fullname)
-}
-function oninpchangesur(e){
-    surname=e.target.value
-    //console.dir('onsurchange: ',surname)  
-    e.stopPropagation(); 
-    
-    fullname=firstname+' '+surname
-    renderName(fullname)
-}
 
-function renderName(f) {
-    console.dir(f)
-    domConResult.textContent=f    
-}
+// console.log(App)
